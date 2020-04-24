bf61189a5026bf82d1102799ada6365e920fcfa9
Yuxin Zhao <Yuxinz11@student.unimelb.edu.au>
2020-04-24 16:26:38 +0800

    Try4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var cafeController = require('../controllers/cafe-controller.js');
+var cafeController = require('../controllers/controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
