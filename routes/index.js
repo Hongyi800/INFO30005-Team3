e44a8e6ff94d32c09fda0b51f22e9fc5fbf79694
Hongyi800 <hongyigu8@gmail.com>
2020-04-25 00:01:06 +0800

    country debug

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
 var countryController = require('../controllers/countryController.js');
-=======
-var myController = require('../controllers/controller.js');
->>>>>>> 8b17cfe8f3a5cdd23bfc339552f98e998d767a8b
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
