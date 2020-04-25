86e181a7cf33906bcc0fe9e97db97b246a9aed05
Yuxin Zhao <Yuxinz11@student.unimelb.edu.au>
2020-04-25 12:25:36 +0800

    Try8

--- app.js
+++ app.js
@@ -62,48 +62,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-//add value of the event into variable preonload
-function addEvent(func) {
-  var preonload = window.onload;
-
-  if (typeof window.onload != 'function') {
-    window.onload = func;
-  } else {
-    window.onload = function () {
-      preonload();
-      func();
-    }
-  }
-}
-
-//receive the comment
-function getMessage() {
-  var comBtn = document.getElementById("btn1");
-  var message = document.getElementById("message");
-  var name = document.getElementById("name");
-  var nameValue = "";
-  var messageValue = "";
-
-  name.onfocus = function () {
-    name.value = "";
-  }
-
-  message.onfocus = function () {
-    message.value = "";
-  }
-
-  comBtn.onclick = function () {
-    messageValue = message.value || "Comment Board";
-    nameValue = name.value || "new user";
-    var messageList = document.getElementById("messageList");
-    var messageDiv = document.createElement("div");
-    var messageText = document.createTextNode(nameValue + ": " + messageValue);
-
-    messageDiv.appendChild(messageText);
-    messageList.appendChild(messageDiv);
-  }
-}
-addEvent(getMessage());
-
 module.exports = app;
 
