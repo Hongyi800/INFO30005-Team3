275a37e69ff2f6620d7b206bed38a44dff3f3303
Yuxin Zhao <Yuxinz11@student.unimelb.edu.au>
2020-04-25 13:16:13 +0800

    Try8

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 var countryController = require('../controllers/countryController.js');
-var myController = require("../controllers/controller.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
