04af1ec6bfad07fd1de3ca6a8e8e89469f34c428
jingg5 <61264376+jingg5@users.noreply.github.com>
2020-04-27 22:11:05 +0800

    Jing's try2

---
+++ routes/commentRoute.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+// create router
+const commentRouter = express.Router();
+
+// load/import the author controller
+const commentController = require("../controllers/informationController.js");
+
+// handle the GET request on root of the author-management path
+// i.e. get all authors
+commentRouter.get("/", commentController.getAllComments);
+
+// handle the POST request to add an author
+commentRouter.post("/", commentController.addComments);
+
+// handle the POST request to update an author
+// note that the PATCH method may be more appropriate
+commentRouter.post("/:id", commentController.updateComments);
+
+// handle the GET request to get an author by ID
+// note that :id refers to a param, accessed by req.params.id in controller fn
+commentRouter.get("/:id", commentController.getCommentsByID);
+
+// export the router
+module.exports = commentRouter;
