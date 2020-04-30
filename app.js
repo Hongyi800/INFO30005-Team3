be7de1dfd79925e1f72cc51ed91830cd61469e4e
Hongyi800 <hongyigu8@gmail.com>
2020-04-30 21:46:33 +0800

    add more html-debug5

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
-//app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'hbs');
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.get("/", (req,res) => {
   res.sendfile("layout.html", {
     root: path.join(__dirname, "../views/")
