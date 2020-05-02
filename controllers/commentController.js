8e65dd29243f39e6639990fc9b96b23d55be9ab2
Hongyi800 <hongyigu8@gmail.com>
2020-05-03 01:44:55 +0800

    add comment page

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,48 +1,10 @@
-//add value of the event into variable preOnload
-const addEvent = (func) => {
-    var preOnload = window.onload;
+const comment = [
+    {"name": "Hongyi", "date": "10 Apr,2020", "body": "this is comment 1"},
+    {"name": "Hongyi2", "date": "8 Apr,2020", "body": "this is comment 2"},
+];
 
-    if (typeof window.onload != 'function') {
-        window.onload = func;
-    } else {
-        window.onload = function() {
-            preOnload();
-            func();
-        }
-    }
-};
 
-//receive the comment
-const getMessage = () => {
-    var comBtn = document.getElementById("btn1");
-    var message = document.getElementById("message");
-    var name = document.getElementById("name");
-    var nameValue = "";
-    var messageValue = "";
-
-    name.onfocus = function () {
-        name.value = "";
-    };
-
-    message.onfocus = function () {
-        message.value = "";
-    };
-
-    comBtn.onclick = function () {
-        messageValue = message.value || "My comment: ";
-        nameValue = name.value || "new user";
-        var messageList = document.getElementById("messageList");
-        var messageDiv = document.createElement("div");
-        var messageText = document.createTextNode(nameValue + ": " + messageValue);
-
-        messageDiv.appendChild(messageText);
-        messageList.appendChild(messageDiv);
-    }
-};
-
-addEvent(getMessage());
-
-module.exports = {
-    addEvent,
-    getMessage
-};
+for(let i=0; i<comment.length; i++){
+    const html = "<div class='commentBox'><div class='leftPanelImg'></div><div class='rightPanel'><span>"+comment[i].name+"</span><div class='date'>"+comment[i].date+"</div><p>"+comment[i].body+"</p></div><div class='clear'></div></div>"
+    $('#container').append(html);
+}
