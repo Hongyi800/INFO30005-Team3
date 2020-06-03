cd4c5ab3d50095762bc84b6a644b7500e2cff18e
Yuxin Zhao <Yuxinz11@student.unimelb.edu.au>
2020-06-03 20:12:42 +0800

    Comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -36,66 +36,8 @@ const addComment = async (req, res) => {
     }
 };
 
-// function to modify comment
-const updateComment = async (req, res) => {
-    const commentId = req.params.username;
-    const new_comment = req.body;
-
-    try {
-        const comments = await Comment.find({id: commentId});
-        if (!comments) {
-            res.status(400);
-            console.log("Comment not found");
-            return res.send("Comment not found");
-        }
-
-        const comment = comments[0];
-        console.log("Comment found!!!", comment);
-
-        comment["content"] = new_comment["content"];
-
-        await comment.save();
-        res.render('initial.pug', {
-            title: 'Comment List'
-        });
-    } catch (err) {
-        res.status(400);
-        console.log(err);
-        return res.send("Database query failed");
-    }
-};
-
-const getCommentByName = async (req, res) => {
-    const commentName = req.params.username;
-
-    try {
-        const comments = await Comment.find({username: commentName});
-        if (!comments) {
-            res.status(400);
-            console.log("Comment not found");
-            return res.send("Comment not found");
-        }
-
-        const comment = comments[0];
-        console.log("Comment found!!!", comment);
-
-
-        res.render('commentupdateform.pug', {
-            title: 'Update Comment',
-            username: comment.username,
-            content: comment.content
-        });
-    } catch (err) {
-        res.status(400);
-        console.log(err);
-        return res.send("Database query failed");
-    }
-};
-
 // remember to export the functions
 module.exports = {
     getAllComments,
-    addComment,
-    updateComment,
-    getCommentByName
+    addComment
 };
--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -10,9 +10,6 @@ commentRouter.get("/", commentController.getAllComments);
 // handle the POST request to add a comment
 commentRouter.post("/", commentController.addComment);
 
-commentRouter.post("/:username", commentController.updateComment);
-
-commentRouter.get("/:username", commentController.getCommentByName);
 // export the router
 module.exports = commentRouter;
 
