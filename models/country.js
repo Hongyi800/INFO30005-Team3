3459ed84feb8501a34e89fd82932fa7fea266af3
Hongyi800 <hongyigu8@gmail.com>
2020-06-04 16:58:16 +0800

    change country model

--- models/country.js
+++ models/country.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const countrySchema = new mongoose.Schema({
   id: String,
   country_name: String,
-  confirmed_case: Number
+  confirmed_case: Number,
+  confirmed_death: Number,
+  Lat: Number,
+  Long_:Number,
 });
 
 const Country = mongoose.model("country", countrySchema, "country");
