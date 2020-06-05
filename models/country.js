f14e1dbe277b7f107c3844ebba0aa25a850783d8
jingg5 <61264376+jingg5@users.noreply.github.com>
2020-06-05 21:34:57 +0800

    update

--- models/country.js
+++ models/country.js
@@ -3,13 +3,10 @@ const mongoose = require("mongoose");
 const countrySchema = new mongoose.Schema({
   country: String,
   confirmed_cases: Number,
-<<<<<<< HEAD
   death_cases: Number,
   latitude: Number,
   longitude:Number,
-=======
   death_cases: Number
->>>>>>> 07c93785fc45a78e8cce215f1dad0075e459b339
 });
 
 const Country = mongoose.model("countries", countrySchema);
